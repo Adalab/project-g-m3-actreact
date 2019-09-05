42b1f659144b74a3b83557a206690b9d57212126
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-09-05 13:21:36 +0200

    Divide Cards into components

---
+++ src/components/CardsHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CardsHeader extends React.Component {
+  render() {
+    return (
+      <header className="cards__header">
+        <div className="container">
+          <h1 className="cards__header-title">Awesome Profile-Cards</h1>
+          <div className="landing__header-logo"></div>
+        </div>
+      </header>
+    
+      );
+    }
+  }
+
+
+export default CardsHeader;
