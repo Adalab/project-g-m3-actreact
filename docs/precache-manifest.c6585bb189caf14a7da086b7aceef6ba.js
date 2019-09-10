aaf92814c1872b007b96637ee2ab9441a67e0b1f
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-09-10 16:31:22 +0200

    create docs

---
+++ docs/precache-manifest.c6585bb189caf14a7da086b7aceef6ba.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048d4bad163ccd673ecb3553d33ebfbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b6830e2362de0863b53",
+    "url": "./static/css/main.6d5927ef.chunk.css"
+  },
+  {
+    "revision": "3a77fcd87379e6b1a135",
+    "url": "./static/js/2.cb5d9d4d.chunk.js"
+  },
+  {
+    "revision": "0b6830e2362de0863b53",
+    "url": "./static/js/main.46ea2e82.chunk.js"
+  },
+  {
+    "revision": "c1a89e668304103dc2ab",
+    "url": "./static/js/runtime~main.7321c57e.js"
+  },
+  {
+    "revision": "370061fd472ae044cfc4a181d8db2192",
+    "url": "./static/media/envelope.370061fd.svg"
+  },
+  {
+    "revision": "7268b2ec68c7e574192a184c80bccfd2",
+    "url": "./static/media/envelope_green.7268b2ec.svg"
+  },
+  {
+    "revision": "dcfe2bd297e0a9c0af6a01600950f573",
+    "url": "./static/media/envelope_red.dcfe2bd2.svg"
+  },
+  {
+    "revision": "239c213dd7dd194f3b34a8df8d4d07b1",
+    "url": "./static/media/github-alt-brands.239c213d.svg"
+  },
+  {
+    "revision": "91778e06ab33daf4e80e49df6a2bbe99",
+    "url": "./static/media/github-alt-brands_green.91778e06.svg"
+  },
+  {
+    "revision": "f86acc1627772f81e62e4fedfcf1f620",
+    "url": "./static/media/github-alt-brands_red.f86acc16.svg"
+  },
+  {
+    "revision": "4a6268137e95cbf146c9e09194ff4012",
+    "url": "./static/media/linkedin-in-brands.4a626813.svg"
+  },
+  {
+    "revision": "968abc282b21922abe5dc6234c6a2e64",
+    "url": "./static/media/linkedin-in-brands_green.968abc28.svg"
+  },
+  {
+    "revision": "645f68641f2b36de146490d3ef7a2976",
+    "url": "./static/media/linkedin-in-brands_red.645f6864.svg"
+  },
+  {
+    "revision": "1a546dcab047a4c5fba0140721aa909e",
+    "url": "./static/media/mobile-alt-solid.1a546dca.svg"
+  },
+  {
+    "revision": "7f5c1e7f7af97931b08e77b5703e6678",
+    "url": "./static/media/mobile-alt-solid_green.7f5c1e7f.svg"
+  },
+  {
+    "revision": "67af776d5bb49f31bdd6fab6e47a16a8",
+    "url": "./static/media/mobile-alt-solid_red.67af776d.svg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/tarjetalogo.0118e311.svg"
+  }
+]);
