0e20b3a2e3abdc89db195fa3470325524a7c42e2
Clara Brígido <clarabrigido@gmail.com>
2019-09-16 11:29:07 +0200

    add LocalStorage

--- src/App.js
+++ src/App.js
@@ -27,9 +27,20 @@ class App extends React.Component {
       this.handleOptionChange = this.handleOptionChange.bind(this)
   }
 
+  componentDidMount(){
+    this.getLocalStorage();
+  }
+
   handleOptionChange(event) {
     const value = parseInt(event.currentTarget.value);
     this.setState(prevState => {
+      const newPalette = {
+        ...prevState.userData,
+        palette: value
+      }; 
+
+      localStorage.setItem('lsUserData', JSON.stringify(newPalette));
+
       return {
         userData: {
           ...prevState.userData,
@@ -42,14 +53,28 @@ class App extends React.Component {
   changePreview(event) {
     const value = event.currentTarget.value;
     const id = event.currentTarget.id;
+
     this.setState(prevState => {
-      return {
-        userData: {
-          ...prevState.userData,
-          [id] : value
-        }
+      const newUserData = {
+        ...prevState.userData, 
+        [id] : value
       };
+        localStorage.setItem('lsUserData', JSON.stringify(newUserData));
+      return {
+        userData: newUserData
+      };
+     
+
      });
+  }
+
+  getLocalStorage() {
+    const ls = JSON.parse(localStorage.getItem('lsUserData'));
+    if ( ls !== null){
+      this.setState({
+        userData:ls
+      });
+    }
   }
   
   render() {
